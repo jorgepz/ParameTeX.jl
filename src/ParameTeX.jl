a48a44e354b6af80b98a38c396dad405a4ab7353
jorgepz <jorgepz@fing.edu.uy>
2021-10-16 16:05:12 -0300

    add DataFrames dependency and adding function to generate pdfs

--- src/ParameTeX.jl
+++ src/ParameTeX.jl
@@ -1,14 +1,30 @@
 module ParameTeX
 
 using CSV
+using DataFrames
 
-greet() = print("Hello World!")
+function readParamFile( fileName )
+  return DataFrame( CSV.File( fileName ) );
+end
 
-function readMyParamFile( fileName )
-  data = CSV.read( fileName )
-  return data
+function generatePdfs( df , folder, baseTeX )
+    dfheaders = names(A);
+    f = open( "../gestion/modeloComprobanteAsistencia/parameters.tex","w") # do this once
+    rows = eachrow(A)
+    cols = eachcol(A)
+
+    print(rows)
+
+    print(cols)
+    for row in rows
+        for col in cols
+            print( "\\newcommand{\\" * dfheaders[col] * "}{" * row[col] * "}\n")
+        end
+    end
+    close(f);
 end
 
 export readParamFile
+export generatePdfs
 
 end # module
