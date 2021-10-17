9321dcd3fb1e1818124c6043282e1cdcdcc98ba5
jorgepz <jorgepz@fing.edu.uy>
2021-10-17 10:26:25 -0300

    added pdf generation function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ParameTeX.jl
+
+a simple parametric generator of pdf files using LaTeX
--- src/ParameTeX.jl
+++ src/ParameTeX.jl
@@ -8,20 +8,42 @@ function readParamFile( fileName )
 end
 
 function generatePdfs( df , folder, baseTeX )
-    dfheaders = names(A);
-    f = open( "../gestion/modeloComprobanteAsistencia/parameters.tex","w") # do this once
-    rows = eachrow(A)
-    cols = eachcol(A)
+    dfheaders = names( df );
+    rows = eachrow( df )
+    numCols = length( dfheaders )
+    paramsFile = joinpath( folder, "parameters.tex" )
 
-    print(rows)
+    currFolder = pwd()
 
-    print(cols)
+    print(" Starts row by row generation...\n")
     for row in rows
-        for col in cols
-            print( "\\newcommand{\\" * dfheaders[col] * "}{" * row[col] * "}\n")
+
+        # open file of parameters and write values
+        paramsFileFid = open( paramsFile , "w" )
+        for col in 1:numCols
+            if typeof(row[col]) == String63
+                valueAsString = replace( row[col], "&"=>"\\&")
+
+            elseif typeof(row[col]) == Int64
+                valueAsString = string( row[col] )
+            else
+                error("type not implemented yet, create and issue!")
+            end
+            write( paramsFileFid, "\\newcommand{\\" * dfheaders[col] * "}{" * valueAsString * "}\n")
         end
+        close( paramsFileFid );
+
+        # compile pdf
+        fileExtension = baseTeX[end-3:end]
+        fileName      = baseTeX[1:end-4]
+        outputPdfFilename = fileName * "_" * replace( row[1]," "=>"_" ) * ".pdf"
+        cd(folder)
+        print(outputPdfFilename,"\n")
+        run(`pdflatex $baseTeX`)
+        run(`mv $fileName.pdf $outputPdfFilename`)
+        cd(currFolder)
     end
-    close(f);
+
 end
 
 export readParamFile
